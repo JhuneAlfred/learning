bc3f11d8e211022485c1586cf732bf0303f56eb6
JhuneAlfred <32024729+JhuneAlfred@users.noreply.github.com>
2018-03-24 22:32:52 -0700

    Update config.js

--- public/sentry_io/config.js
+++ public/sentry_io/config.js
@@ -5,7 +5,7 @@ import Raven from 'raven-js';
 const sentry_key = '';
 const sentry_app = '';
 export const sentry_url = `https://${sentry_key}@sentry.io/${sentry_app}`;
-//Raven.config('https://a32114b6536c40c299b55d832c91158f@sentry.io/227992').install()
+
 
 export function logException(ex, context) {
     Raven.captureException(ex, {
