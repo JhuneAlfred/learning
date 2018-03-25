8f2ff15132fae78afa265940e4af1535e9189e34
JhuneAlfred <32024729+JhuneAlfred@users.noreply.github.com>
2018-03-24 22:32:38 -0700

    Update config.js

--- public/sentry_io/config.js
+++ public/sentry_io/config.js
@@ -2,8 +2,8 @@
 
 import Raven from 'raven-js';
 
-const sentry_key = 'a32114b6536c40c299b55d832c91158f';
-const sentry_app = '227992';
+const sentry_key = '';
+const sentry_app = '';
 export const sentry_url = `https://${sentry_key}@sentry.io/${sentry_app}`;
 //Raven.config('https://a32114b6536c40c299b55d832c91158f@sentry.io/227992').install()
 
